204554abb83b749b2f7874562c26327c6d6fa439
Satyajit Gokhale <twisterintwist@gmail.com>
2024-10-02 11:02:53 -0400

    Automated transformation to reduce re-rendering

--- wiki/src/components/ShopCart.tsx
+++ wiki/src/components/ShopCart.tsx
@@ -1,42 +1,32 @@
+import { memo } from "react";
 import { useStore } from "@nanostores/react";
 import { itemsStorage, addCartItem } from "../stores/cartStorage";
 import { MaterialType, GivePreset, Preset, PresetType } from "../types";
 import PresetContainer from "./PresetContainer";
-
 const givePreset: GivePreset = {
   name: "xd",
   "custom-model": 0,
   amount: 1,
   lore: ["a"],
   slot: -1,
-  material: MaterialType.ACACIA_LEAVES,
+  material: MaterialType.ACACIA_LEAVES
 };
-
 const give = new Preset(PresetType.GivePreset, givePreset);
-
-export default function ShopCart() {
+export default memo(function ShopCart() {
   const items = useStore(itemsStorage);
-
   function click() {
     addCartItem(give);
   }
-
-  const listPresets = Object.keys(items).map((key) => {
+  const listPresets = Object.keys(items).map(key => {
     const reward = items[key];
     return <PresetContainer id={key} item={reward} />;
   });
-
-  return (
-    <div className="w-full px-2">
+  return <div className="w-full px-2">
       <div>
-        <button
-          onClick={click}
-          className="text-tkm_white bg-tkm_purple_dark p-3 rounded-md active:bg-tkm_purple transition-all"
-        >
+        <button onClick={click} className="text-tkm_white bg-tkm_purple_dark p-3 rounded-md active:bg-tkm_purple transition-all">
           Add item!
         </button>
       </div>
       <div>{listPresets}</div>
-    </div>
-  );
-}
+    </div>;
+});
